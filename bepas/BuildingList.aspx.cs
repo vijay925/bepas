a18dc6cfab95cf7f8ea13e4203749df828760a1c
vijay925 <vijay925.phs@gmail.com>
2016-08-22 11:51:25 -0700

    implemented Site List, Building List

---
+++ bepas/BuildingList.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace bepas
+{
+    public partial class WebForm6 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                LoadSiteList();
+                LoadBuildingList(4);
+
+            } //if
+        }
+
+        private void LoadBuildingList(int siteUid)
+        {
+            DataSet dataSet = GetDataUsingSp("spLoadBuildings", "@siteUid", siteUid);
+            gvBuildingList.DataSource = dataSet;
+            gvBuildingList.DataBind();
+            gvBuildingList.HeaderRow.TableSection = TableRowSection.TableHeader;
+        } //LoadBuildingList()
+
+        private void LoadSiteList()
+        {
+            DataSet dataSet = GetDataUsingSp("spLoadSites", null, null);
+            gvSiteList.DataSource = dataSet;
+            gvSiteList.DataBind();
+            gvSiteList.HeaderRow.TableSection = TableRowSection.TableHeader;
+        } //LoadSiteList()
+
+        protected void gvSiteListOnRowCommandSelect(object sender, GridViewCommandEventArgs e)
+        {
+            string[] argument = new string[3];
+            argument = e.CommandArgument.ToString().Split(';');
+
+            string siteUidLocal = argument[0];
+            string siteIdByUserLocal = argument[1];
+            string siteNameLocal = argument[2];
+
+            siteId.Text = siteIdByUserLocal;
+            siteName.Text = siteNameLocal;
+            LoadBuildingList(Convert.ToInt32(siteUidLocal));
+        }
+
+        private DataSet GetDataUsingSp(string spName, string spParameterName, object spParameter)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["bepas"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.CommandText = spName;
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Connection = connection;
+
+                DataSet dataSet = new DataSet();
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    if (spParameter != null)
+                        command.Parameters.AddWithValue(spParameterName, (int)spParameter);
+                    connection.Open();
+                    adapter.Fill(dataSet);
+                } //using SqlDataAdapter
+                return dataSet;
+            } //using SqlCommand
+        } //GetDataUsingSp()
+
+
+    }
+}
--- bepas/SiteList.aspx
+++ bepas/SiteList.aspx
@@ -26,7 +26,7 @@
         </div>
     </div>
 
-        <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.1.0/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.1.0/js/responsive.bootstrap.min.js"></script>
@@ -36,7 +36,7 @@
             $('#<%=gvSiteList.ClientID%>').DataTable({
                 "aLengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
                 "pageLength": 5,
-                
+
                 /*
                 "columns": [{ "sName": "uid" }, { "sName": "siteIdByUser" }, { "sName": "siteName" },
                             { "sName": "surveyDate" }, { "sName": "contactName" }, { "sName": "city" },
