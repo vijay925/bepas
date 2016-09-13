84afc6c27461487f7243dba06b2f104612cf45ec
vijay925 <vijay925.phs@gmail.com>
2016-09-12 20:28:59 -0700

    began implementing MiscInventory

---
+++ bepas/MiscInventoryDetail.aspx.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace bepas
+{
+    public partial class MiscInventoryDetail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                LoadSiteList();
+
+            } //if
+
+        } //Page_Load()
+
+
+        private void LoadSiteList()
+        {
+            DataSet dataSet = GetDataUsingSp("spLoadSites", null, null);
+            gvSiteList.DataSource = dataSet;
+            gvSiteList.DataBind();
+            gvSiteList.HeaderRow.TableSection = TableRowSection.TableHeader;
+        } //LoadSiteList()
+
+        protected void gvSiteListOnRowCommandSelect(object sender, GridViewCommandEventArgs e)
+        {
+            SuccessPanel.Visible = false;
+            buildingId.Text = String.Empty;
+            buildingName.Text = String.Empty;
+
+            string[] argument = new string[3];
+            argument = e.CommandArgument.ToString().Split(';');
+
+            string siteUidLocal = argument[0];
+            string siteIdByUserLocal = argument[1];
+            string siteNameLocal = argument[2];
+
+            siteId.Text = siteIdByUserLocal;
+            siteName.Text = siteNameLocal;
+            LoadBuildingList(Convert.ToInt32(siteUidLocal));
+        }
+
+        private void LoadBuildingList(int siteUid)
+        {
+            DataSet dataSet = GetDataUsingSp("spLoadBuildings", "@siteUid", siteUid);
+            gvBuildingList.DataSource = dataSet;
+            gvBuildingList.DataBind();
+            gvBuildingList.HeaderRow.TableSection = TableRowSection.TableHeader;
+        } //LoadBuildingList()
+
+        private void LoadRoomList(int buildingUid)
+        {
+            DataSet dataSet = GetDataUsingSp("spLoadRoomList", "@buildingUid", buildingUid);
+            gvRoomList.DataSource = dataSet;
+            gvRoomList.DataBind();
+            gvRoomList.HeaderRow.TableSection = TableRowSection.TableHeader;
+        } //LoadBuildingList()
+
+        private void LoadInventoryList(int roomUid)
+        {
+            DataSet dataSet = GetDataUsingSp("spLoadMiscInventoryList", "@roomUid", roomUid);
+            gvRoomList.DataSource = dataSet;
+            gvRoomList.DataBind();
+            gvRoomList.HeaderRow.TableSection = TableRowSection.TableHeader;
+        } //LoadBuildingList()
+
+        protected void gvBuildingListOnRowCommandSelect(object sender, GridViewCommandEventArgs e)
+        {
+            string[] argument = new string[3];
+            argument = e.CommandArgument.ToString().Split(';');
+            SuccessPanel.Visible = false;
+
+            string buildingUidLocal = argument[0];
+            string buildingIdByUserLocal = argument[1];
+            string buildingNameLocal = argument[2];
+
+            buildingId.Text = buildingIdByUserLocal;
+            buildingName.Text = buildingNameLocal;
+            LoadRoomList(Convert.ToInt32(buildingUidLocal));
+        }
+
+        protected void gvRoomListOnRowCommandSelect(object sender, GridViewCommandEventArgs e)
+        {
+            string[] argument = new string[3];
+            argument = e.CommandArgument.ToString().Split(';');
+            SuccessPanel.Visible = false;
+
+            string roomUidLocal = argument[0];
+            string roomIdByUserLocal = argument[1];
+            string roomNameLocal = argument[2];
+
+            roomId.Text = roomIdByUserLocal;
+            roomName.Text = roomNameLocal;
+            LoadInventoryList(Convert.ToInt32(roomUidLocal));
+        }
+
+        protected void gvInventoryListOnRowCommandSelect(object sender, GridViewCommandEventArgs e)
+        {
+            string[] argument = new string[2];
+            argument = e.CommandArgument.ToString().Split(';');
+            SuccessPanel.Visible = false;
+
+            string inventoryUidLocal = argument[0];
+            string inventoryNameLocal = argument[1];
+
+            ViewState["inventoryUid"] = inventoryUidLocal;
+            inventoryName.Text = inventoryNameLocal;
+            LoadInputFields(Convert.ToInt32(inventoryUidLocal));
+        }
+
+        private void LoadInputFields(int buildingUid)
+        {
+            DataSet dataSet = GetDataUsingSp("spLoadBuildingGeneralInfo", "@buildingUid", buildingUid);
+
+            if (dataSet.Tables[0].Rows.Count > 0)
+            {
+                DataRow dr = dataSet.Tables[0].Rows[0];
+                //yearBuilt.Text = dr["yearBuilt"].ToString(); ;
+                //ddlBuildingEndUse.SelectedValue = dr["buildingEndUseId"].ToString();
+                //radioBoxedShape.SelectedValue = dr["boxedShapeId"].ToString();
+                //grossAreaPerFloor.Text = dr["grossAreaPerFloor"].ToString();
+                //buildingHeight.Text = dr["buildingHeight"].ToString();
+                //buildingWidth.Text = dr["buildingWidth"].ToString();
+                //buildingLength.Text = dr["buildingLength"].ToString();
+                //numberOfHVAC.Text = dr["numberOfHVAC"].ToString();
+                //radioOwnedOrLeased.SelectedValue = dr["ownedOrLeasedId"].ToString();
+                //numberOfFloors.Text = dr["numberOfFloors"].ToString();
+                //radioPreviousAudit.SelectedValue = dr["previousAuditId"].ToString();
+                //previousAuditDate.Text = dr["previousAuditDate"].ToString();
+                //meterId.Text = dr["meterId"].ToString();
+                //radioMeteredIndividually.SelectedValue = dr["meteredIndividuallyId"].ToString();
+                //notes.Value = dr["notes"].ToString();
+            }
+            else
+            {
+                ClearInputFields();
+            }
+        } //LoadInputFields()
+
+        private DataSet GetDataUsingSp(string spName, string spParameterName, object spParameter)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["bepas"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.CommandText = spName;
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Connection = connection;
+
+                DataSet dataSet = new DataSet();
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    if (spParameter != null)
+                        command.Parameters.AddWithValue(spParameterName, (int)spParameter);
+                    connection.Open();
+                    adapter.Fill(dataSet);
+                } //using SqlDataAdapter
+                return dataSet;
+            } //using SqlCommand
+        } //GetDataUsingSp()
+
+        protected void cancelButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~");
+        }
+
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid) //checks validation again in case javascript disabled <-- havent tested this yet
+            {
+
+                string connectionString = ConfigurationManager.ConnectionStrings["bepas"].ConnectionString;
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    int UserUid = 1;
+                    command.CommandText = "spInsertMiscInventory";
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Connection = connection;
+                    command.Parameters.AddWithValue("@roomUid", Convert.ToInt32(ViewState["roomUid"]));
+                    //command.Parameters.AddWithValue("@name", name.Text);
+                    command.Parameters.AddWithValue("@make", make.Text);
+                    command.Parameters.AddWithValue("@model", model.Text);
+                    command.Parameters.AddWithValue("@quantity", quantity.Text);
+                    command.Parameters.AddWithValue("@wattage", wattage.Text);
+                    command.Parameters.AddWithValue("@purpose", purpose.InnerText);
+                    command.Parameters.AddWithValue("@runTime", runTime.Text);
+                    command.Parameters.Add("@unitPhoto", SqlDbType.VarBinary).Value = DBNull.Value;
+                    command.Parameters.AddWithValue("@unitPhotoFileName", DBNull.Value);
+                    command.Parameters.AddWithValue("@notes", notes.InnerText);
+                    command.Parameters.AddWithValue("@userId", UserUid);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    SuccessPanel.Visible = true;
+                }
+            } // if(page valid)
+
+        } //addButton_Click()
+
+        private void ClearInputFields()
+        {
+            inventoryName.Text = String.Empty;
+            make.Text = String.Empty;
+            model.Text = String.Empty;
+            quantity.Text = String.Empty;
+            wattage.Text = String.Empty;
+            purpose.Value = String.Empty;
+            runTime.Text = String.Empty;
+            notes.Value = String.Empty;
+        }
+
+    } //Webform
+} //namespace bepas
